42be72b259398aaa1a85d30f6dda8b814d9c88a7
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-02-13 22:38:53 +0530

    Added a PUT for student attendance

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class Student(BaseModel):
     srn: str
     semester: Optional[float] = None
 
+class Student_Min(BaseModel):
+    name: Optional[str] = None
+    srn: str
+    semester: Optional[float] = None
+
 @app.get("/")
 async def root():
     res = "Hey there! Welcome to the Attendance App of PESU I/O Course 'Cloud Native Full Stack Development'"
@@ -24,4 +29,11 @@ async def new_student(student: Student, response: Response):
     except:
         response.status_code = 403
     
-    
+@app.put("/student", status_code=200)
+async def student_attendance(student: Student_Min, response: Response):
+    json_student = jsonable_encoder(student)
+    try:
+        print("works")
+        # TODO send the data to db
+    except:
+        response.status_code = 403
