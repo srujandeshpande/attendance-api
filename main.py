af793547e836d105ceb9c4af29b0c83a7a69307a
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-02-15 22:54:00 +0530

    Modifications

--- main.py
+++ main.py
@@ -29,13 +29,22 @@ async def new_student(student: Student, response: Response):
     except:
         response.status_code = 403
     
-@app.put("/student", status_code=200)
+@app.put("/attend", status_code=200)
 async def student_attendance(student: Student_Min, response: Response):
     json_student = jsonable_encoder(student)
     try:
         srn = student['srn']
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # TODO send the data to db
+    except:
+        response.status_code = 403
+
+@app.get("/student", status_code=200)
+async def get_attendance(student: Student, response: Response):
+    json_student = jsonable_encoder(student)
+    try:
+        print("works")
         # TODO send the data to db
     except:
         response.status_code = 403
