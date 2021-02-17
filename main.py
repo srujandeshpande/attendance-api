85d0de3f93f0518acee5b504f75cae5f02464917
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-02-17 22:12:06 +0530

    Format SRN to Uppercase

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ async def new_student(student: Student, response: Response):
 @app.put("/attend", status_code=200)
 async def student_attendance(student: Student_Min, response: Response):
     json_student = jsonable_encoder(student)
+    srn = json_student['srn'].upper()
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     try:
-        srn = student['srn']
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         # TODO send the data to db
     except:
         response.status_code = 403
@@ -43,6 +43,7 @@ async def student_attendance(student: Student_Min, response: Response):
 @app.get("/student", status_code=200)
 async def get_attendance(student: Student, response: Response):
     json_student = jsonable_encoder(student)
+    srn = json_student['srn'].upper()
     try:
         print("works")
         # TODO send the data to db
